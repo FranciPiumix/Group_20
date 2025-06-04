11fdb095742baf16c68e91d585b28a83aa0ab6e5
FranciPiumix <kicca.tede@gmail.com>
2025-06-04 11:28:51 +0200

    aggiunto file js a tutti

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Ingrandisce i link del menu
 document.querySelectorAll('#header nav a').forEach(link => {
 	link.addEventListener('mouseover', () => {
 		link.style.transform = 'scale(1.2)';
@@ -6,4 +7,14 @@ document.querySelectorAll('#header nav a').forEach(link => {
 	link.addEventListener('mouseout', () => {
 		link.style.transform = 'scale(1)';
 	});
+});
+
+// Ingrandisce l'icona home
+const homeIcon = document.querySelector('#top-right-nav a');
+homeIcon.addEventListener('mouseenter', () => {
+	homeIcon.style.transform = 'scale(1.3)';
+	homeIcon.style.transition = 'transform 0.2s ease';
+});
+homeIcon.addEventListener('mouseleave', () => {
+	homeIcon.style.transform = 'scale(1)';
 });
