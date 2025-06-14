c31a539bed0683e69a92244d730998f2fdcb7737
matimonti01 <mati.monti01@gmail.comgit>
2025-06-14 15:40:59 +0200

    grafico2

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,36 @@
 // assets/js/map.js
 
+let osm = new Tile({
+    title: "Open Street Map",
+    type: "base",
+    visible: true,
+    source: new OSM()
+});
+// Add the layer groups code here:
+let basemapLayers = new Group({
+    title: 'Base Maps',
+    layers: [osm]
+});
+let overlayLayers = new Group({
+    title: 'Overlay Layers',
+    layers: [
+ ]
+});
+
+// Add the map controls here:
+map.addControl(new ScaleLine());
+map.addControl(new FullScreen());
+map.addControl(
+    new MousePosition({
+        coordinateFormat: createStringXY(4),
+        projection: 'EPSG:4326',
+        className: 'custom-control',
+        placeholder: '0.0000, 0.0000'
+    })
+);
+
+
 // Coordinate centro della Repubblica Ceca
 const czechCenter = [49.8175, 15.4730];
 
 const map = L.map('map').setView(czechCenter, 7);
-
-// Aggiunge tile base OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
