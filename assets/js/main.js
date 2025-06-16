8276c73d8925adf49e9652f741082a788343ca55
FranciPiumix <kicca.tede@gmail.com>
2025-06-16 10:18:54 +0200

    modifiche a css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 // Ingrandisce i link del menu
 document.querySelectorAll('#header nav a').forEach(link => {
-	link.addEventListener('mouseover', () => {
-		link.style.transform = 'scale(1.2)';
-		link.style.transition = 'transform 0.2s';
-	});
-	link.addEventListener('mouseout', () => {
-		link.style.transform = 'scale(1)';
-	});
+    link.addEventListener('mouseover', () => {
+        link.style.transform = 'scale(1.2)';
+        link.style.transition = 'transform 0.2s';
+    });
+    link.addEventListener('mouseout', () => {
+        link.style.transform = 'scale(1)';
+    });
 });
 
 // Ingrandisce l'icona home
 const homeIcon = document.querySelector('#top-right-nav a');
 homeIcon.addEventListener('mouseenter', () => {
-	homeIcon.style.transform = 'scale(1.3)';
-	homeIcon.style.transition = 'transform 0.2s ease';
+    homeIcon.style.transform = 'scale(1.3)';
+    homeIcon.style.transition = 'transform 0.2s ease';
 });
 homeIcon.addEventListener('mouseleave', () => {
-	homeIcon.style.transform = 'scale(1)';
+    homeIcon.style.transform = 'scale(1)';
 });
